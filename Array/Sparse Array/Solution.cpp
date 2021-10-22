f59906381057196f8f9beb9e62bcb04183f3f5d9
Sumit Saha <Sumit360saha@gmail.com>
2021-10-23 01:25:00 +0530

    Update Solution.cpp

--- Array/Sparse Array/Solution.cpp
+++ Array/Sparse Array/Solution.cpp
@@ -3,21 +3,31 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
-
-int main() {
-    int n,d;
-    cin>>n>>d;
-    int a[n];
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    for(int i=d;i<n;i++){
-        cout<<a[i]<<" ";
-    }
-    for(int i=0;i<d;i++){
-        cout<<a[i]<<" ";
+int main()
+{
+  int n;
+  cin>>n;
+    string a[1000],b;
+    for(int i=0;i<n;i++)
+   {
+      cin>>b;
+      a[i]=b;
+     }
+    int q;
+    cin>>q;
+    string check;
+    for(int i=0;i<q;i++)
+    {int count=0;
+     cin>>check;    
+       for(int j=0;j<n;j++)
+       {
+          if(check.compare(a[j])==0)
+              count++;
+       }
+     cout<<count<<endl;
     }
     return 0;
 }
